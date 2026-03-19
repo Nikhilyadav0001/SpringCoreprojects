a201f014171116ea299161b025c24c96e5e1ae47
nikhilyadav0001 <nikhilraoshab1@gmail.com>
2026-03-20 03:51:04 +0530

    dependency lookup

---
+++ 15-SpringAnnotationConfig-DL/src/main/java/nikhil/bean/CricketBat.java
@@ -0,0 +1,24 @@
+package nikhil.bean;
+
+import java.util.Random;
+
+import org.springframework.stereotype.Component;
+
+@Component("bat")
+public class CricketBat {
+
+	static {
+		System.out.println("CricketBat.class file is loading..");
+	}
+
+	public CricketBat() {
+		System.out.println("CricketBat Object is created...");
+	}
+
+	//BL
+	public int scoreRuns() {
+		System.out.println("CricketBat.scoreRuns()");
+		return new Random().nextInt(200);
+	}
+
+}
